72af90a92b8f09359007f9a0cc2d27915c8c3fbd
John-Enquist <enquistjj@gmail.com>
2023-06-20 13:49:32 -0400

    updated codebase to support sqlalchemy 2.0, also added error logging

--- etlcore/DB/DB.py
+++ etlcore/DB/DB.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.engine.base import Engine
 
 class DB():
@@ -15,9 +15,9 @@ class DB():
             print("successfully connected to the database")
             return con
         except Exception as e:
-            print(f"unable to connect to db {str(e)}")
+            return f"unable to connect to db {str(e)}"
 
-    def change_db_connection(self, con_str: str):
+    def change_db_connection(self, con_str: str) -> bool:
         self.engine = create_engine(con_str, fast_executemany=True, execution_options={"isolation_level": "AUTOCOMMIT"})
         return True
 
@@ -27,39 +27,40 @@ class DB():
             df.to_sql(name = f"RAW_{table_name}", dtype= dtype_dict, con = self.engine, schema = schema, if_exists = "replace", index=False, chunksize=chunksize)
             return True
         except Exception as e:
-            print(f"insert failed {str(e)}")        
+            return f"insert failed {str(e)}"
+
 
     def kill_fill(self, stage_db: str, dest_schema: str, table_name: str) -> bool:
         try:
             with self.engine.connect() as con:
-                con.execute(f"EXECUTE dbo.spETL_KillFillDSQL @table = '{table_name}', @org = '{self.org}', @stage_db = '{stage_db}', @dest_schema='{dest_schema}'")
+                con.execute(text(f"EXECUTE dbo.spETL_KillFillDSQL @table = '{table_name}', @org = '{self.org}', @stage_db = '{stage_db}', @dest_schema='{dest_schema}'"))
             return True
         except Exception as e:
-            print(f"kill/fill of table failed: {str(e)}")
+            return f"kill/fill of table failed: {str(e)}"
 
     def run_proc(self, db: str, schema: str, stored_procedure: str) -> bool:
         try:
             with self.engine.connect() as con:
-                result = con.execute(f"EXECUTE {db}.{schema}.{stored_procedure}").fetchall()
+                result = con.execute(text(f"EXECUTE {db}.{schema}.{stored_procedure}")).fetchall()
                 for q in result[0]:
                     if q == 1:
-                        print(f"Query{list(result[0]).index(q) + 1} has failed")
-                        return False
+                        return f"Query{list(result[0]).index(q) + 1} has failed"
+                        #return False
             return True
         except Exception as e:
-            print(f"stored procedure run {stored_procedure} failed! error string: {str(e)}")
+            return f"stored procedure run {stored_procedure} failed! error string: {str(e)}"
 
     def run_proc_with_param(self, db: str, schema: str, stored_procedure: str, param: str) -> pd.DataFrame:
         try:
             with self.engine.connect() as con:
-                result = con.execute(f"EXECUTE {db}.{schema}.{stored_procedure} {param}").fetchall()
+                result = con.execute(text(f"EXECUTE {db}.{schema}.{stored_procedure} {param}")).fetchall()
                 for q in result[0]:
                     if q == 1:
-                        print(f"Query{list(result[0]).index(q) + 1} has failed")
-                        return False
+                        return f"Query{list(result[0]).index(q) + 1} has failed"
+                        #return False
             return True
         except Exception as e:
-            print(f"stored procedure run {stored_procedure} failed! error string: {str(e)}")
+            return f"stored procedure run {stored_procedure} failed! error string: {str(e)}"
     
     def upsert(self):
         raise NotImplementedError
