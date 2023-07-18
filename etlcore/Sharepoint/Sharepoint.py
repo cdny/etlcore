78c24142dfbc2a9315d58b7c99a4947cc712597d
John-Enquist <enquistjj@gmail.com>
2023-07-18 18:20:13 +0000

    added download functionality for sharepoint class

--- etlcore/Sharepoint/Sharepoint.py
+++ etlcore/Sharepoint/Sharepoint.py
@@ -4,7 +4,6 @@ from office365.runtime.auth.client_credential import ClientCredential
 from office365.runtime.auth.user_credential import UserCredential
 from office365.sharepoint.client_context import ClientContext
 
-
 class Sharepoint():
     def __init__(self, auth_type: str, credentials: UserCredential | ClientCredential, site: str):
         
@@ -32,3 +31,13 @@ class Sharepoint():
             return True
         except Exception as e:
             return f"Failed to upload file {local_file_path} to sharepoint: {str(e)}"
+        
+    def download_file_from_sharepoint(self, sp_folder: str, remote_filename: str, local_file_path: str):
+        try:
+            remote_path = os.path.join(sp_folder, remote_filename) #file on sharepoint
+            destination_path = os.path.join(local_file_path, os.path.basename(remote_filename)) #destination location for file
+            with open(destination_path, "wb") as local_file:
+                file = self.sp_ctx_connection.web.get_file_by_server_relative_url(remote_path).download(local_file).execute_query()
+                return True
+        except Exception as e:
+            return f"Failed to download file {remote_filename} from {sp_folder} to {local_file_path}"
