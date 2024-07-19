acb33931eec432dee588ad65116978315608aab7
Jason Noxon <jnoxon@caredesignny.org>
2024-07-19 18:11:36 +0000

    added function to get file and return binary object

--- etlcore/BlobStorage/BlobStorage.py
+++ etlcore/BlobStorage/BlobStorage.py
@@ -1,3 +1,4 @@
+import io
 from azure.storage.blob import BlobServiceClient
 
 
@@ -6,7 +7,15 @@ class BlobStorage:
     def __init__(self, connection_string: str) -> None:
 
         try:
-            service = BlobServiceClient.from_connection_string(
+            self.service = BlobServiceClient.from_connection_string(
                 conn_str=connection_string)
         except:
-            print("There was an issue connecting to Blob Storage")
+            print("The connection to blob storage failed")
+
+    def get_file(self, file_path: str, container_name):
+        
+        blob_client = self.service.get_blob_client(container_name, file_path)
+        stream = io.BytesIO()
+        blob_client.download_blob().readinto(stream)
+        
+        return stream
