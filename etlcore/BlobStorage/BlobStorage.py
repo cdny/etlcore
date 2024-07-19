eea6f00fd80209ebd77e57e00c5ef084453e3921
Jason Noxon <jnoxon@caredesignny.org>
2024-07-19 16:38:45 +0000

    Added azure blob library and excluded test.py from git

--- etlcore/BlobStorage/BlobStorage.py
+++ etlcore/BlobStorage/BlobStorage.py
@@ -1,3 +1,12 @@
+from azure.storage.blob import BlobServiceClient
+
+
 class BlobStorage:
-    def __init__(self) -> None:
-        pass
+
+    def __init__(self, connection_string: str) -> None:
+
+        try:
+            service = BlobServiceClient.from_connection_string(
+                conn_str=connection_string)
+        except:
+            print("There was an issue connecting to Blob Storage")
