5b3d2ba9ef9bff7d549ee267850c38bb6f802b97
ntyme <ntymeson@caredesignny.org>
2024-08-01 11:14:58 -0400

    Added upload_blob method to BlobStorage

--- etlcore/BlobStorage/BlobStorage.py
+++ etlcore/BlobStorage/BlobStorage.py
@@ -26,3 +26,11 @@ class BlobStorage:
         blob_client = self.blob_service_client.get_blob_client(
             container_name, blob_name)
         blob_client.delete_blob()
+
+    def upload_blob(self, container_name: str, blob_name: str, file_path: str, overwrite: bool = False):
+
+        blob_client = self.blob_service_client.get_blob_client(
+            container_name, blob_name
+        )
+        with open(file_path, "rb") as file_content:
+            blob_client.upload_blob(file_content.read(), overwrite=overwrite)
