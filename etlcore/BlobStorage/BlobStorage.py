c1c3e8d3559bf6bb01c2922d90a58b10aa0b33ad
ntyme <ntymeson@caredesignny.org>
2024-08-01 14:27:38 -0400

    Updated upload_blob params

--- etlcore/BlobStorage/BlobStorage.py
+++ etlcore/BlobStorage/BlobStorage.py
@@ -27,10 +27,10 @@ class BlobStorage:
             container_name, blob_name)
         blob_client.delete_blob()
 
-    def upload_blob(self, container_name: str, blob_name: str, file_path: str, overwrite: bool = False):
+    def upload_blob(self, container_name: str, file_path: str, overwrite: bool = False):
 
         blob_client = self.blob_service_client.get_blob_client(
-            container_name, blob_name
+            container_name, file_path
         )
         with open(file_path, "rb") as file_content:
             blob_client.upload_blob(file_content.read(), overwrite=overwrite)
