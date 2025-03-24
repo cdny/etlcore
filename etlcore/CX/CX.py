557351453e8f2f9bfe5d457479cbece68bd08282
John-Enquist <enquistjj@gmail.com>
2025-03-24 09:18:37 -0400

    add logout to cx functions, added string table def handling

--- etlcore/CX/CX.py
+++ etlcore/CX/CX.py
@@ -39,13 +39,10 @@ class CX:
 
         return response.ok
     
-    #This method ends session with Medisked and initializes a countdown timer
+    #This method ends session with Medisked
     def logout(self):
         response = self.session.get("{}/Account/Logout".format(self.URL))
-        print(response.status_code)
-
-        if response.status_code == 302:
-            self.isLoggedIn = False
+        self.isLoggedIn = False
         return response.status_code
     
     def refreshSession(self) -> None:
